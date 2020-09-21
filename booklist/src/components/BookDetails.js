81765bd5835432910e1568ef4d7038c7a73689e2
Shameel Fazul <shameel.fazul@mail.com>
2020-09-21 17:04:57 +0530

    Reducers
    - Dispatches & Actions
    - Added Reducer
    - useEffect Hook to add data to Local Storage
    - Get data from Local Storage and return data to books as intitial value.

--- booklist/src/components/BookDetails.js
+++ booklist/src/components/BookDetails.js
@@ -2,9 +2,10 @@ import React, { useContext } from 'react';
 import { BookContext } from '../contexts/BookContext';
 
 const BookDetails = ({book}) => { //Destructuring props.book
-    const { removeBook } = useContext(BookContext);
+    //const { removeBook } = useContext(BookContext);
+    const { dispatch } = useContext(BookContext);
     return (
-        <li onClick={ (e) => removeBook(book.id) }>  {/* <li> tag because this component is nested in a <ul> tag */}
+        <li onClick={ (e) => dispatch({ type: 'REMOVE_BOOK', id: book.id }) }>  {/* <li> tag because this component is nested in a <ul> tag */}
             <div className="title">{ book.title }</div>
             <div className="author">{ book.author }</div>
         </li>
--- booklist/src/contexts/BookContext.js
+++ booklist/src/contexts/BookContext.js
@@ -1,26 +1,40 @@
-import React, { createContext, useState } from 'react';
-import uuid from 'uuid/dist/v1';
+import React, { createContext, useReducer, useEffect } from 'react';
+import { bookReducer } from '../reducers/bookReducer';
 
 export const BookContext = createContext();
 
 const BookContextProvider = (props) => {
-    const [books, setBooks] = useState([
-        { title: 'name of the wind', author: 'patrick rothfuss', id: 1},
-        { title: 'the final empire', author: 'brandon sanderson', id: 2}
-    ]);
 
-    const addBook = (title, author) => {
-        setBooks([...books, { title: title, author: author, id: uuid() }]);
-    };
+    const [books, dispatch] = useReducer(bookReducer, [], () => { // "[] : Second Argument" is the value for the books variable. In thise case, it's blank.
+        const localData = localStorage.getItem('books');
+        return localData ? JSON.parse(localData) : [];
+        // The third argument is for the default value. In this case, it's a function.
+        // Get the data from the local storage and return data to the "books" variable.
+    }); 
+                                                           
 
-    const removeBook = (id) => {
-        setBooks(books.filter((book) => book.id !== id ));
-    };
+    useEffect(() => { // 'useEffect' hook will run everytime the app initializes or data in "books" changes.
+        localStorage.setItem('books', JSON.stringify(books));
+        // The value property in the local storage requires the data type to be a String.
+        // ^ Since we can't add a JavaScript object to the value property
+        // ^ We use "JSON.stringify" to make the object into a string.
+    },[books]);
 
+    // const [books, setBooks] = useState([
+    //     { title: 'name of the wind', author: 'patrick rothfuss', id: 1},
+    //     { title: 'the final empire', author: 'brandon sanderson', id: 2}
+    // ]);
 
+    // const addBook = (title, author) => {
+    //     setBooks([...books, { title: title, author: author, id: uuid() }]);
+    // };
+
+    // const removeBook = (id) => {
+    //     setBooks(books.filter((book) => book.id !== id ));
+    // };
 
     return (
-        <BookContext.Provider value={ {books, addBook, removeBook} }>
+        <BookContext.Provider value={ {books, dispatch} }>
             { props.children }
         </BookContext.Provider>
     );
